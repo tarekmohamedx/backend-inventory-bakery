881357e55a6d06e78c1fcb524bcfa749a9eb2c06
Eman Sameh <emansameh143@gmail.com>
2025-01-31 07:20:28 +0200

    edit connection on database

--- database/mongo/index.js
+++ database/mongo/index.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
-
-
 const dotenv = require('dotenv').config();
 
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.MONGO_URL)
-    .then(()=>{
+const connectDatabase = async (callback) => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('DB Connected Successfully');
-    }).catch((err)=>{
-            console.log(`Error Connecting to DB: ` + err);
-    })
-}
+        if (callback) callback();
+    } catch (err) {
+        console.log(`Error Connecting to DB: ` + err);
+    }
+};
 
 module.exports.DATA_BASE = {connectDatabase};
-
-
 
--- main.js
+++ main.js
@@ -3,15 +3,13 @@ const { DATA_BASE } = require("./database/mongo/index");
 const { APP_CONFIG } = require("./config/db");
 const NodeJSTest = require("./index");
 
+
 (async function () {
   // connecting to database
-  await DATA_BASE.connectDatabase({
-    uri: APP_CONFIG.MONGO_URL,
-    callback: () => {
-      console.log("App database has connected successfully");
-      NodeJSTest.listen(APP_CONFIG.HTTP_PORT, "0.0.0.0", () => {
-        console.log(`App is up and running on port ${APP_CONFIG.HTTP_PORT}`);
-      });
-    },
+  await DATA_BASE.connectDatabase(() => {
+    console.log("App database has connected successfully");
+    NodeJSTest.listen(APP_CONFIG.HTTP_PORT, "0.0.0.0", () => {
+      console.log(`App is up and running on port ${APP_CONFIG.HTTP_PORT}`);
+    });
   });
 })();
