b92a46cda70e31b3ece31e0ba2764440cd1062ca
marinawafik14 <marinawafiq15@gmail.com>
2025-01-31 11:58:31 +0200

    some changes

--- database/mongo/index.js
+++ database/mongo/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv').config();
 const connectDatabase = ()=>{
     mongoose.connect(process.env.MONGO_URL)
     .then(()=>{
-        console.log('DB Connected Successfully');
+        console.log(`DB Connected Successfully on port ${process.env.HTTP_PORT}`);
     }).catch((err)=>{
             console.log(`Error Connecting to DB: ` + err);
     })
