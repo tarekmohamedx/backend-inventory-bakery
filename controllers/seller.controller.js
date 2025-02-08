d4d98cabe3eac5a9c5f81cee524f113865ba6271
marinawafik14 <marinawafiq15@gmail.com>
2025-02-08 11:22:06 +0200

    api chart seller

---
+++ controllers/seller.controller.js
@@ -0,0 +1,80 @@
+const express =require('express');
+const sellerService = require("../services/seller.service");
+
+
+    const router =express.Router();
+    // get all sellers
+    router.get("/sellers", async (req, res) => {
+        try {
+            const sellers = await sellerService.getSellers();
+            res.status(200).json(sellers)
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    });
+
+    // get seller by id
+    router.get("/seller/:id", async (req, res) => {
+        try {
+            const seller = await sellerService.getSellerById(req.params.id)
+            if (!seller) return res.status(404).json({ error: 'seller not found' })
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+    })
+
+    // create seller
+    router.post("/sellers", async (req, res) => {
+        try {
+            const newSeller = await sellerService.createSeller(req.body)
+            res.status(201).json(newSeller)
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+
+    })
+
+
+    // update seller
+    router.post("/sellers/:id", async (req, res) => {
+        try {
+            const updateSeller = await sellerService.updateSeller(req.params.id, req.body)
+            if (!updateSeller) return res.status(404).json({ error: 'Seller not found' });
+            res.status(201).json(updateSeller)
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+
+    })
+
+    // delete seller
+    router.post("/sellers/:id", async (req, res) => {
+        try {
+            const deleteSeller = await sellerService.deleteSeller(req.params.id)
+            if (!deleteSeller) return res.status(404).json({ error: 'Seller not found' });
+            res.status(201).json({ message: 'Seller deleted successfully' })
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+
+    })
+
+    // Get seller stats
+    router.get('/seller-stats', async (req, res) => {
+        try {
+            const sellers = await sellerService.getSellers();
+            const stats = sellers.map(seller => ({
+                storeName: seller.storeName,
+                totalSales: seller.totalSales,
+                totalProfits: seller.totalProfits,
+            }));
+            res.status(200).json(stats);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    });
+
+
+
+
+    module.exports= router;
---
+++ repos/seller.repo.js
@@ -0,0 +1,69 @@
+
+const Seller =require( '../models/seller.model');
+
+
+//get all sellers
+ const getsellers = async()=>{
+    try{
+return await Seller.find({})
+
+    }catch(error){
+        console.error('Error fetching sellers:', error);
+        throw error;
+    }
+}
+
+// get seller by id
+
+ const getSellerById =async(sellerID)=>{
+try{
+return await Seller.findById(sellerID)
+
+}catch(error){
+    console.error('Error fetching seller by ID:', error);
+    throw error;
+}
+
+}
+
+// create a new seller
+ const createSeller =async(sellerData)=>{
+    try{
+        const newSeller = new Seller(sellerData);
+        return await newSeller.save();
+
+    }catch(error){
+        console.log("Error in creating seller",error);
+        throw Error
+    }
+}
+
+//update seller
+ const updateSeller = async (sellerID,updateData)=>{
+try{
+return await Seller.findByIdAndUpdate(sellerID,updateData,{new:true,runValidators: true})
+
+}catch(error){
+    console.log("Error in updating seller",error);
+        throw Error
+}
+
+}
+
+// delete seller
+const deleteSeller = async(sellerID)=>{
+    try{
+return await Seller.findByIdAndDelete(sellerID);
+    }catch(error){
+        console.log("Error in deleting seller",error);
+        throw Error
+    }
+}
+
+module.exports = {
+    getsellers,
+    getSellerById,
+    createSeller,
+    updateSeller,
+    deleteSeller
+};
