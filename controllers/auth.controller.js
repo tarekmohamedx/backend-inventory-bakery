717755b93af58e9c21132d88b89d435a0aa8f894
Eman Sameh <emansameh143@gmail.com>
2025-02-20 07:44:52 +0200

    commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,82 +1,143 @@
+const { registerUser, loginUser , decode } = require("../services/auth.service");
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const { mergeGuestCart } = require('../services/cart.service')
 
-const appError = require('../utils/appError');
-const httpStatusText = require('../utils/httpStatusText');
-const asyncwrapper = require('../middlewere/asyncWrapper');
-const bcrypt = require('bcrypt');
-const Users = require('../models/users.model');
-const jwt = require('jsonwebtoken');
+const routes = {
+  register: async (req, res) => {
+    try {
+      userbody = req.body;
 
-const getAllUser = asyncwrapper(async (req, res, next) => {
-    const query = req.query;
-    const limit = query.limit || 10;
-    const page = query.page || 1;
-    const skip = (page - 1) * limit;
-    const users = await Users.find({}, { "__v": false }).limit(limit).skip(skip);
-    res.json({ status: httpStatusText.SUCCESS, data: { users } });
-});
+      console.log("userbody", userbody);
+      // Call registerUser service to handle user registration and get the token
+      const { token } = await registerUser(userbody);
 
-const Register = asyncwrapper(async (req, res, next) => {
-    const { firstName, lastName, email, password ,role} = req.body;
-
-    const oldUser = await Users.findOne({ email: email });
-    if (oldUser) {
-        const error = appError.create('User already exists', 400, httpStatusText.FAIL);
-        return next(error);
+      req.session.token = token;
+      // Send success response with the token
+      
+      res.status(201).json({ success: true, token });
+    } catch (error) {
+      console.error("Error in register:", error.message);
+      res
+        .status(500)
+        .json({ message:error.message || "Failed to register user" });  
     }
 
-    //! Password hashing
-    const hashedPassword = await bcrypt.hash(password, 10);
+  },
 
-    const newUser = new Users({
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-        role,
-    });
-      //generate token
-    const token= await jwt.sign({ email:newUser.email,id:newUser._id,role:newUser.role} , process.env.JWT_SECRET_KEY,{expiresIn:'50d'});
-    console.log('token',token);
-    newUser.token=token;
+    login: async (req, res) => {
+      try {
+        const { email, password , guestCart = []} = req.body; // Directly use the entire body as user credentials
 
+        // Call loginUser service to handle user login and get the token
+        const { token , user} = await loginUser({ email, password });
 
-    await newUser.save();
-    res.status(201).json({ status: httpStatusText.SUCCESS, data: { user: newUser } });
-});
+      // **🔄 Merge Guest Cart with User's Cart**
+      const updatedCart = await mergeGuestCart(user._id, guestCart);  // Call mergeGuestCart
+      console.log("✅ Merged Cart:", updatedCart);
 
-const Login = asyncwrapper(async (req, res, next) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        const error = appError.create('Email and password are required', 400, httpStatusText.FAIL);
-        return next(error);
-    }
-
-    const user = await Users.findOne({ email: email });
-    if (!user) {
-        const error = appError.create('Invalid email or password', 400, httpStatusText.FAIL);
-        return next(error);
-    }
-
-    const matchedPassword = await bcrypt.compare(password, user.password);
-    if(user && matchedPassword){
-        //logged in successfully
-        const token= await jwt.sign({ email:user.email,id:user._id,role:user.role} , process.env.JWT_SECRET_KEY,{expiresIn:'50d'});
-        res.json({status:httpStatusText.SUCCESS,data:{token:user.token}})
-    }
-    else{
-        const error =appError.create('Something Wrong',500,httpStatusText.ERROR);
-        return next(error);
-    }
-});
-
-module.exports = {
-    getAllUser,
-    Register,
-    Login
-};
+        // Send success response with the token
+        res.status(200).json({ token });
+      } catch (error) {
+        console.error("Error in login:", error.message);
+        res
+          .status(500)
+          .json({ message: error.message || "Failed to login user" });
+      }
+    },
 
 
+}
+
+
+router.post("/auth/register", routes.register);
+router.post("/auth/login", routes.login);
+// router.get('/auth/decode',routes.decoding);
+module.exports = router;
+
+
+
+
+
+// const appError = require('../utils/appError');
+// const httpStatusText = require('../utils/httpStatusText');
+// const asyncwrapper = require('../middlewere/asyncWrapper');
+// const bcrypt = require('bcrypt');
+// const Users = require('../models/users.model');
+// const jwt = require('jsonwebtoken');
+
+// const getAllUser = asyncwrapper(async (req, res, next) => {
+//     const query = req.query;
+//     const limit = query.limit || 10;
+//     const page = query.page || 1;
+//     const skip = (page - 1) * limit;
+//     const users = await Users.find({}, { "__v": false }).limit(limit).skip(skip);
+//     res.json({ status: httpStatusText.SUCCESS, data: { users } });
+// });
+
+// const Register = asyncwrapper(async (req, res, next) => {
+//     const { firstName, lastName, email, password ,role} = req.body;
+
+//     const oldUser = await Users.findOne({ email: email });
+//     if (oldUser) {
+//         const error = appError.create('User already exists', 400, httpStatusText.FAIL);
+//         return next(error);
+//     }
+
+//     //! Password hashing
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     const newUser = new Users({
+//         firstName,
+//         lastName,
+//         email,
+//         password: hashedPassword,
+//         role,
+//     });
+//       //generate token
+//     const token= await jwt.sign({ email:newUser.email,id:newUser._id,role:newUser.role} , process.env.JWT_SECRET_KEY,{expiresIn:'50d'});
+//     console.log('token',token);
+//     newUser.token=token;
+
+
+//     await newUser.save();
+//     res.status(201).json({ status: httpStatusText.SUCCESS, data: { user: newUser } });
+// });
+
+// const Login = asyncwrapper(async (req, res, next) => {
+//     const { email, password } = req.body;
+
+//     if (!email || !password) {
+//         const error = appError.create('Email and password are required', 400, httpStatusText.FAIL);
+//         return next(error);
+//     }
+
+//     const user = await Users.findOne({ email: email });
+//     if (!user) {
+//         const error = appError.create('Invalid email or password', 400, httpStatusText.FAIL);
+//         return next(error);
+//     }
+
+//     const matchedPassword = await bcrypt.compare(password, user.password);
+//     if(user && matchedPassword){
+//         //logged in successfully
+//         const token= await jwt.sign({ email:user.email,id:user._id,role:user.role} , process.env.JWT_SECRET_KEY,{expiresIn:'50d'});
+//         res.json({status:httpStatusText.SUCCESS,data:{token:user.token}})
+//     }
+//     else{
+//         const error =appError.create('Something Wrong',500,httpStatusText.ERROR);
+//         return next(error);
+//     }
+// });
+
+// module.exports = {
+//     getAllUser,
+//     Register,
+//     Login
+// };
+
+// --------------------------------------------------------------------------------------------/
 // const Users=require('../models/users.model');
 // const appError = require('../utils/appError');
 // const httpStatusText = require('../utils/httpStatusText')
@@ -143,3 +204,4 @@ module.exports = {
 
 //     getAllUser,Register,Login
 // }const Users = require('../models/users.model');
+
